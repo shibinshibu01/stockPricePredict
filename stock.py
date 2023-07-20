5882eb7be48792756efa1d3dc795f9186b3b2cd7
Shibin Shibu <shibinsb01@gmail.com>
2023-07-21 03:59:58 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,7 +1,7 @@
-from sklearn.impute import SimpleImputer
 import pandas as pd
 import numpy as np
 import yfinance as yf
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
